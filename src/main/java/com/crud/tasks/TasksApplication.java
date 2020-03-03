22443ec7de2621beeddef043cf4eb44090340e5e
Aleks0491 <aleksander.sotor@gmail.com>
2020-03-03 20:51:25 +0100

    Zad 19.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,7 +3,9 @@ package com.crud.tasks;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
 public class TasksApplication {
 
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,7 +1,12 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,17 +17,29 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/v1/task", consumes = "application/json")
+@RequestMapping(value = "/v1/task", consumes = MediaType.APPLICATION_JSON_VALUE)
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
+
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTask());
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "testTitle", "test_content");
+//    @RequestMapping(method = RequestMethod.GET, value = "getTaskById")
+//    public List<TaskDto> getTasksByIds(@RequestBody Long taskId) {
+//        return taskMapper.mapToTaskDtoList(service.getTasksByIds(taskId));
+//    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTaskById")
+    public TaskDto getTask(@RequestBody Long taskId) {
+        //return new TaskDto(1L, "testTitle", "test_content");
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
